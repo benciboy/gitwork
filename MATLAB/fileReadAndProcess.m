376bcafede02198823b71d246536dce9d51f43a2
Benedek Kocsis <benedek.kocsis@ericsson.com>
2011-05-16 16:38:46 +0200

    looks like work for simple case

--- MATLAB/fileReadAndProcess.m
+++ MATLAB/fileReadAndProcess.m
@@ -1,16 +1,36 @@
    
 % fajlok beolvasasa es feldolgozasa
-     d=dir('d*'); % <- retrieve all names: file(s) and folder(s)
+     d=dir('saf_*'); % <- retrieve all names: file(s) and folder(s)
      d=d(~[d.isdir]); % <- keep file name(s), only
      d={d.name}.'; % <- file name(s)
      nf=numel(d);
 for i=1:nf
-     fprintf('working on %d/%d: %s\n',i,nf,d{i});
-     M = dlmread(d{i},'\n'); 
+     fprintf('working on %d/%d: %s\n',i,nf,d{i}); 
+     A = dlmread(d{i},'\n'); 
+     na = A(1,1);
+     A(1,:) = [];
+     for j=i+1:nf
+        B = dlmread(d{j},'\n'); 
+        nb = B(1,1);
+        B(1,:) = [];      
+       [h,p, max] = kstest2_cum(A,B,na,nb);
+       fprintf('H = %d, P = %f, Dmax = %f \n',h,p,max); 
+   % disp (  [h,p, max] );
+     end
+     %disp(M);
 end
-disp(M);
+%disp(M);
 
-A = dlmread('data.txt','\n');
-B = dlmread('data1.txt','\n');
-[h,p] = kstest2(A,B);
-disp([h,p]);
+A = dlmread('saf_3_1','\n');
+B = dlmread('saf_3_3','\n');
+nb = B(1,1);
+ na = A(1,1);
+A(1,:) = [];
+B(1,:) = [];
+%nb = B(1,1);
+% na = A(1,1);
+[h,p, max] = kstest2_cum(A,B,na,nb);
+disp([h,p,max]);
+
+%M(1,:) = [];
+%disp(M);
